27a80cd74ebcfeeb22ac630c05c6f14166dcd446
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-20 04:19:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,17 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
+
     time = Time.new
- 
-    if time < 12 
+
+    if time < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
     end
- 
+
     resp.finish
   end
- 
+
 end
