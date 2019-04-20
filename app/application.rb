4d37f72fa2cb08b965d23e1cea5ba311e1b459a5
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-20 04:25:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new
-
+    time = Time.now.hour
+    
     if time < 12
-      resp.write "Good Morning"
+      resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
     binding.pry
 
